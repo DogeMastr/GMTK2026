6d4e1240b89552c464acafe6dfc2fd0dce9fd5f2
augustinjanuary <0701brian04@gmail.com>
2026-07-23 15:56:38 +0100

    Debug player movement

--- Scenes/Mole/mole.gd
+++ Scenes/Mole/mole.gd
@@ -7,6 +7,7 @@ var y_pos = 0
 
 func _ready() -> void:
 	EventBus.move_down.connect(move_mole_down)
+	EventBus.mole_dies.connect(death)
 
 func _process(delta: float) -> void:
 	# PLAYER INPUTS
@@ -22,6 +23,18 @@ func _process(delta: float) -> void:
 	
 	if Input.is_action_just_pressed("player_down"):
 		move_mole_down(1)
+	
+	#No card player movement
+	if Input.is_action_just_pressed("1"):
+		move_mole_down(1)
+	if Input.is_action_just_pressed("2"):
+		move_mole_down(2)
+	if Input.is_action_just_pressed("3"):
+		move_mole_down(3)
+	if Input.is_action_just_pressed("4"):
+		move_mole_down(4)
+	if Input.is_action_just_pressed("5"):
+		move_mole_down(5)
 	pass
 
 func move_mole_down(amount_to_travel: int):
@@ -30,7 +43,12 @@ func move_mole_down(amount_to_travel: int):
 			var tile_to_travel_to = EventBus.get_tile(x_pos, y_pos + i + 1)
 			if tile_to_travel_to.type == 5:
 				amount_to_travel = i
+			if tile_to_travel_to.type == 4 or tile_to_travel_to.type == 3:
+				amount_to_travel = i + 1
+				EventBus.mole_dies.emit()
 			
 		position.y += EventBus.tile_width * amount_to_travel
 		y_pos += amount_to_travel
 		
+func death():
+	print("dead")
